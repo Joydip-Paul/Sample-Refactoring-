664fb510c44c017d2a07a5620d7b514fec6ffe41
Joydip Paul <joydippaul72@gmail.com>
2021-10-28 14:06:44 +0600

    new branch

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
                 <img src={hero} alt="" />
                 <div className="overlay">
                     <div className="hero-content">
-                        <small>HOW IT WORKS</small>
+                        <small>HOW IT WORKS Hello</small>
                         <h1>Your daily dose of health <br />in 3 simple steps</h1>
                         <p>It is a long established fact that a reader will be distracted readable content<br /> by the of a page when looking at its layout. </p>
                         <a href="#!" className= "primary-btn2">Be Member</a>
